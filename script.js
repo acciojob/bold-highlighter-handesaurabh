69c1bc55c0fa65a004b2aed8496065b70367876d
handesaurabh <handesaurabh2004@gmail.com>
2025-10-19 16:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	const mouseOver=document.querySelectorAll("strong");
+mouseOver.forEach((item)=>{
+	item.style.color="rgb(0, 128, 0)";
+});
 }
 
 
 function return_normal() {
     //Write your code here
-
+const mouseOut=document.querySelectorAll("strong");
+mouseOut.forEach((item)=>{
+	item.style.color="rgb(0, 0, 0)";
+});
     
 }
